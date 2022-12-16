9dc2eaecf7b405015b0f6cba1b7178dbd2330fba
Lukas Koeller <acc.koeller@protonmail.com>
2022-12-16 13:11:46 +0100

    feat: add base structure with canvas

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,17 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
+  <main>
+    <header>
+      <h1>Achtung die Kurve</h1>
+    </header>
+    <canvas></canvas>
+    <menu>
+      <button type="button">Starten</button>
+    </menu>
+  </main>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const canvas = document.querySelector('canvas');
+console.log(canvas);
+

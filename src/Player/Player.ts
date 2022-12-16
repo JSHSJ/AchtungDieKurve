b8cae56c79db7cb2e7c34495d6a0a5254fd4350f
Lukas Koeller <acc.koeller@protonmail.com>
2022-12-16 17:36:58 +0100

    feat: add methods to Player

--- src/Player/Player.ts
+++ src/Player/Player.ts
@@ -1,50 +1,86 @@
 import { TPosition } from "../types/TPosition";
 import { TDirection } from "../types/TDirection";
-import { updateRotationValue } from "../util/updateRotationValue";
+import { updateRotationValueSin, updateRotationValueCos } from "../util/updateRotationValue";
+import { PLAYER_WIDTH, BASE_SPEED } from "../config/config";
+import { TControls } from "../types/TControls";
+import { doPointsIntersect } from "../util/doPointsIntersect";
 
-export class Player {
-  private id: string;
-  public isAlive: boolean;
-  public color: string;
-  private direction: TDirection;
-  public currentPosition: TPosition;
-  public previousPosition: TPosition[];
-  private #counter = 0;
-
-  constructor(
+export type PlayerArgs = {
     id: string,
     color: string,
     startPosition: TPosition,
-    startDirection: TDirection
-  ) {
-    this.id = id;
-    this.color = color;
-    this.currentPosition = startPosition;
-    this.direction = startDirection;
+    startCounter: number,
+    controls: TControls,
+};
+
+export class Player {
+  public id: string;
+  public isAlive: boolean;
+  public color: string;
+  private direction: TDirection = { x: 0, y: -1 };
+  public currentPosition: TPosition;
+  public previousPositions: TPosition[];
+  private counter = 0;
+  private controls: TControls;
+
+  constructor(args: PlayerArgs) {
+    this.id = args.id;
+    this.color = args.color;
+    this.currentPosition = args.startPosition;
+    this.counter = args.startCounter;
+    this.controls = args.controls;
+
     this.isAlive = true;
-    this.previousPosition = [];
-    this.previousPosition.push(this.currentPosition);
+    this.previousPositions = [];
+    this.previousPositions.push(args.startPosition);
+    this.updateDirection();
   }
 
   public move() {
+    if (!this.isAlive) return;
     this.currentPosition.x += this.direction.x * BASE_SPEED;
     this.currentPosition.y += this.direction.y * BASE_SPEED;
-    this.previousPosition.push(this.currentPosition);
   }
 
-  public setAlive(isAlive: boolean) {
-    this.isAlive = isAlive;
+  public die() {
+    this.isAlive = false;
   }
 
-  public rotateLeft() {
-    this.#counter -= 1;
-    this.direction.y = updateRotationValue(this.#counter);
-    this.direction.x = updateRotationValue(this.#counter + Math.PI);
+  public turn(keys: Set<string> | undefined) {
+    if (keys?.has(this.controls.left)) {
+        this.counter -= 1;
+        this.updateDirection();
+    }
+    if (keys?.has(this.controls.right)) {
+        this.counter += 1;
+        this.updateDirection();
+    }
   }
 
-  public rotateRight() {
-    this.#counter += 1;
-    this.direction.y = updateRotationValue(this.#counter);
-    this.direction.x = updateRotationValue(this.#counter + Math.PI);
+  private updateDirection() {
+    this.direction.y = updateRotationValueSin(this.counter);
+    this.direction.x = updateRotationValueCos(this.counter + Math.PI)
+  }
+
+  public draw(ctx: CanvasRenderingContext2D) {
+    if (!this.isAlive) return;
+
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(
+      this.currentPosition.x,
+      this.currentPosition.y,
+      PLAYER_WIDTH,
+      0,Math.PI * 2, false
+    );
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  public updatePreviousPositions() {
+    const previousPoint = this.previousPositions.at(-1);
+    if (!previousPoint || !doPointsIntersect(previousPoint, this.currentPosition)) {
+    this.previousPositions.push(Object.assign({}, this.currentPosition));
+    }
   }
 }

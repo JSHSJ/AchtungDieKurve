ee7bfbd3e8bdf0cee86f4d59703c4b7df3ab8c13
Lukas Koeller <acc.koeller@protonmail.com>
2022-12-16 17:35:21 +0100

    feat: add config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,12 @@
-const BASE_SPEED = 1;
-const BASE_ROTATION = 0.01;
+export const BASE_SPEED = 0.5;
+export const BASE_ROTATION = 0.01;
+export const PLAYER_WIDTH = 2;
+
+export const PLAYER_COLORS = [
+  '#ae3ec9',
+  '#4263eb',
+  '#0ca678',
+  '#74b816',
+  '#f59f00',
+  '#f76707',
+]

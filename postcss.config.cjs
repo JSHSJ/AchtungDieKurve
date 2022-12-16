a5e7ceae0615a213c0fe0c7fbbb528e816cc336a
Lukas Koeller <acc.koeller@protonmail.com>
2022-12-16 13:11:24 +0100

    chore: add postcss config

---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+const postcssPresetEnv = require('postcss-preset-env');
+// import postcssPresetEnv from 'postcss-preset-env';
+
+module.exports = {
+  plugins: [
+    postcssPresetEnv({ stage: 1 }),
+  ],
+};

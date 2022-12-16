6d5ca5af593eef9fd155612e18ab0321820d8284
Lukas Koeller <acc.koeller@protonmail.com>
2022-12-16 20:25:24 +0100

    chore: improve update fn

--- src/util/updateRotationValue.ts
+++ src/util/updateRotationValue.ts
@@ -1,9 +1,9 @@
-import { BASE_ROTATION } from '../config/config';
+import { BASE_TURNING_RADIUS } from '../config/config';
 
 export const updateRotationValueSin = (time: number) =>
-  Math.round(1000 * (1 + Math.sin(2 * Math.PI * BASE_ROTATION * time) - 1)) /
+  Math.round(1000 * (1 + Math.sin(2 * Math.PI * BASE_TURNING_RADIUS * time) - 1)) /
   1000;
 
 export const updateRotationValueCos = (time: number) =>
-  Math.round(1000 * (1 + Math.cos(2 * Math.PI * BASE_ROTATION * time) - 1)) /
+  Math.round(1000 * (1 + Math.cos(2 * Math.PI * BASE_TURNING_RADIUS * time) - 1)) /
   1000;

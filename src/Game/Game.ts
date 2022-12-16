9cf501869b5dc1aae81515f1382468683f34cb14
Lukas Koeller <acc.koeller@protonmail.com>
2022-12-16 15:05:39 +0100

    refactor: simplify Game

--- src/Game/Game.ts
+++ src/Game/Game.ts
@@ -1,23 +1,16 @@
 export class Game {
+    public ctx: CanvasRenderingContext2D;
     public width: number;
     public height: number;
-    public ctx: CanvasRenderingContext2D | null;
 
-    constructor() {
-        const canvas = document?.querySelector('canvas');
-        if (!canvas) throw Error(
-            'Cannot find `<canvas>` element. Make sure to add it to `index.html`.'
-        );
-        this.ctx = canvas.getContext("2d");
-        if (this.ctx === null) throw Error('Canvas context (`ctx`) is not defined.');
-
-        this.width = canvas.offsetWidth ?? 500;
-        this.height = canvas.offsetHeight ?? 500;
-        console.log(this.width, this.height);
+    constructor(ctx: CanvasRenderingContext2D) {
+        this.ctx = ctx;
+        this.width = ctx.canvas.width;
+        this.height = ctx.canvas.height;
     }
 
     animate() {
-        this?.ctx?.clearRect(0, 0, this.width, this.height);
+        this.ctx.clearRect(0, 0, this.width, this.height);
         // loop here
         requestAnimationFrame(this.animate);
     }

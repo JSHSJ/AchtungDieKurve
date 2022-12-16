bb20f55e1118a0acccd1b7311024c5559944a5cc
Lukas Koeller <acc.koeller@protonmail.com>
2022-12-16 13:55:38 +0100

    feat: add constructor and animate

--- src/Game/Game.ts
+++ src/Game/Game.ts
@@ -1,7 +1,29 @@
 export class Game {
     public width: number;
     public height: number;
+    public ctx: CanvasRenderingContext2D | null;
 
+    constructor() {
+        const canvas = document?.querySelector('canvas');
+        if (!canvas) throw Error(
+            'Cannot find `<canvas>` element. Make sure to add it to `index.html`.'
+        );
+        this.ctx = canvas.getContext("2d");
+        if (this.ctx === null) throw Error('Canvas context (`ctx`) is not defined.');
+
+        this.width = canvas.offsetWidth ?? 500;
+        this.height = canvas.offsetHeight ?? 500;
+        console.log(this.width, this.height);
+    }
+
+    animate() {
+        this.ctx.clearRect(0, 0, this.width, this.height);
+        for (let i = 0; i < particleArray.length; i++) {
+            particleArray[i].update();
+            particleArray[i].draw();
+        }
+        requestAnimationFrame(animate);
+    }
 
     public checkCollisions() {
 

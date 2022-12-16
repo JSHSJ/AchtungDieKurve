c719bedf8b054fc9c55ac0de07907cb26e63d460
Lukas Koeller <acc.koeller@protonmail.com>
2022-12-16 17:34:00 +0100

    chore: add updateRotationValueCos

--- src/util/updateRotationValue.ts
+++ src/util/updateRotationValue.ts
@@ -1,3 +1,9 @@
-export const updateRotationValue = (time: number) =>
+import { BASE_ROTATION } from '../config/config';
+
+export const updateRotationValueSin = (time: number) =>
   Math.round(1000 * (1 + Math.sin(2 * Math.PI * BASE_ROTATION * time) - 1)) /
+  1000;
+
+export const updateRotationValueCos = (time: number) =>
+  Math.round(1000 * (1 + Math.cos(2 * Math.PI * BASE_ROTATION * time) - 1)) /
   1000;

fe701a45b145784b1b4638ba4d918deef6ce809c
Lukas Koeller <acc.koeller@protonmail.com>
2022-12-18 19:49:09 +0100

    chore: structure app and add action bar and score

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-    import { PLAYER_COLORS } from "./config/config";
-    import logoSvg from './assets/logo.svg';
-    import { onMount } from "svelte";
-    import { Game } from "./Game/Game";
-    import { Player } from "./Player/Player";
+    import { PLAYER_COLORS } from './config/config';
+    import { onMount } from 'svelte';
+    import { Game } from './Game/Game';
+    import { Player } from './Player/Player';
 
     const DEFAULT_CANVAS_SIZE = 500;
 
@@ -26,7 +25,7 @@
         if (!ctx) throw Error('Canvas context (ctx) is not defined');
         ctx.canvas.width = canvas?.offsetWidth ?? DEFAULT_CANVAS_SIZE;
         ctx.canvas.height = canvas?.offsetHeight ?? DEFAULT_CANVAS_SIZE;
-    })
+    });
 
     let numberOfPlayers = 2;
 
@@ -41,50 +40,52 @@
         console.log(formData.get(`player[0][color]`));
 
         Array(numberOfPlayers)
-        .fill(0)
-        .forEach((_player, idx) => {
-            players.push(
-                new Player({
-                    id: formData.get(`player[${idx}][name]`) as string,
-                    color: formData.get(`player[${idx}][color]`) as string,
-                    startPosition: () => ({
-                        x: Math.random() * (ctx!.canvas.width - 100) + 50,
-                        y: Math.random() * (ctx!.canvas.height - 100) + 50,
+            .fill(0)
+            .forEach((_player, idx) => {
+                players.push(
+                    new Player({
+                        id: formData.get(`player[${idx}][name]`) as string,
+                        color: formData.get(`player[${idx}][color]`) as string,
+                        startPosition: () => ({
+                            x: Math.random() * (ctx!.canvas.width - 100) + 50,
+                            y: Math.random() * (ctx!.canvas.height - 100) + 50,
+                        }),
+                        controls: {
+                            left: formData.get(`player[${idx}][controls][left]`) as string,
+                            right: formData.get(`player[${idx}][controls][right]`) as string,
+                        },
                     }),
-                    controls: {
-                        left: formData.get(`player[${idx}][controls][left]`) as string,
-                        right: formData.get(`player[${idx}][controls][right]`) as string,
-                    },
-                }),
-            );
-        });
+                );
+            });
 
         startGame();
     }
 </script>
 
 <main>
-  <div class="canvas-wrapper">
-      <canvas></canvas>
-  </div>
-  <aside>
-      <img class="logo" src={logoSvg} alt="Achtung die Kurve">
-      <form on:submit|preventDefault={handleSubmit}>
-          <div class="form-rows">
+    <section>
+        <canvas />
+    </section>
+    <aside>
+        <header>
+            <h1>Achtung die Kurve</h1>
+        </header>
+        <form on:submit|preventDefault={handleSubmit} name="main">
             {#each Array(numberOfPlayers).fill(0) as _player, idx}
-              <fieldset class="player-row" name="player">
+                <fieldset name="player">
                     <input
                         type="color"
                         name="player[{idx}][color]"
                         bind:value={PLAYER_COLORS[idx]}
-                    >
+                    />
                     <div class="input-wrapper">
                         <label for="player[{idx}][name]">Your name</label>
                         <input
-                            type="text" required
+                            type="text"
+                            required
                             name="player[{idx}][name]"
                             id="player[{idx}][name]"
-                        >
+                        />
                     </div>
                     <div class="input-wrapper">
                         <label for="player[{idx}][controls][left]">Left</label>
@@ -94,7 +95,7 @@
                             id="player[{idx}][controls][left]"
                             maxlength="1"
                             required
-                        >
+                        />
                     </div>
                     <div class="input-wrapper">
                         <label for="player[{idx}][controls][right]">Right</label>
@@ -104,19 +105,17 @@
                             type="text"
                             maxlength="1"
                             required
-                        >
+                        />
                     </div>
-              </fieldset>
+                    <div class="score">
+                        <span>Score</span>
+                    </div>
+                </fieldset>
             {/each}
-            <button
-              type="button"
-              class="add"
-              on:click={addPlayerRow}
-            >
-              Add +
-            </button>
-          </div>
-          <button type="submit">Start</button>
-      </form>
-  </aside>
+        </form>
+        <div class="action-bar">
+            <button type="button" class="add" on:click={addPlayerRow}> Add Player</button>
+            <button class="start" form="main" type="submit">Start</button>
+        </div>
+    </aside>
 </main>

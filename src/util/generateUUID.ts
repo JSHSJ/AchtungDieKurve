58eb85abbb4f7eb07adbedea7a211954e7ee29b6
Lukas Koeller <acc.koeller@protonmail.com>
2023-01-02 17:27:40 +0100

    fix: handle crypto defined but empty object cases
    Resolved #34

--- src/util/generateUUID.ts
+++ src/util/generateUUID.ts
@@ -1,5 +1,5 @@
 export const generateUUID = (prefix: string = '') => {
-    if (typeof crypto === 'undefined') {
+    if (typeof crypto === 'undefined' || Object.keys(crypto).length === 0) {
         return `${prefix}-${Math.random().toString()}`;
     }
     return `${prefix}-${crypto.randomUUID()}`;

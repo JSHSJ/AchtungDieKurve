26b15b9cd1024229d519760769dd7bfe3b0626ea
JSHSJ <kontakt@joshuastuebner.com>
2023-01-14 21:06:16 +0100

    fix: fix rounds counting all sorts of false collisions

--- src/modules/Round/Round.ts
+++ src/modules/Round/Round.ts
@@ -9,7 +9,10 @@ export class Round extends EventEmitter<RoundEvent> {
     canvas: Canvas;
     players: Player[];
     #playerRanking: Player[] = [];
+    #currentRankIdx = 0;
     state: RoundState = RoundState.PRE_ROUND;
+
+    animationHandle = 0;
 
     private readonly keys: Set<string>;
 
@@ -25,6 +28,8 @@ export class Round extends EventEmitter<RoundEvent> {
         this.canvas = canvas;
         this.players = players;
         this.keys = keys;
+        this.#playerRanking = new Array(players.length);
+        this.#currentRankIdx = players.length - 1;
     }
 
     public start() {
@@ -40,11 +45,12 @@ export class Round extends EventEmitter<RoundEvent> {
      * Throttle the game loop to 60 fps
      */
     private runGameLoopWrapper() {
-        if (this.state !== RoundState.RUNNING) {
+        if (this.state === RoundState.ROUND_OVER) {
+            cancelAnimationFrame(this.animationHandle);
             return;
         }
 
-        requestAnimationFrame(this.runGameLoopWrapper.bind(this));
+        this.animationHandle = requestAnimationFrame(this.runGameLoopWrapper.bind(this));
 
         this.now = Date.now();
         this.delta = this.now - this.then;
@@ -56,6 +62,10 @@ export class Round extends EventEmitter<RoundEvent> {
     }
 
     private runGameLoop() {
+        if (this.state === RoundState.ROUND_OVER) {
+            return;
+        }
+
         this.players.forEach((player) => {
             player.move();
             player.turn(this.keys);
@@ -80,19 +90,17 @@ export class Round extends EventEmitter<RoundEvent> {
 
     public checkCollisions(): Collision[] {
         return this.players
+            .filter((player) => player.isAlive)
             .map((player) => this.canvas.didPlayerCollide(player, this.players))
-            .reduce((acc, collision) => {
-                if (collision) {
-                    acc.push(collision);
-                }
-                return acc;
-            }, [] as Collision[]);
+            .filter((collision): collision is Collision => !!collision);
     }
 
     private handleCollisions(collisions: Collision[]): void {
         collisions.forEach((collision) => {
             // Add dead player to the front of the players
-            this.#playerRanking = [collision.player, ...this.#playerRanking];
+            if (collision.player.isAlive) {
+                this.addPlayerToRanking(collision.player);
+            }
             if (collision?.type === 'PLAYER_COLLISION') {
                 this.emit({
                     type: RoundEventTypes.PLAYER_COLLISION,
@@ -122,17 +130,24 @@ export class Round extends EventEmitter<RoundEvent> {
             return false;
         }
 
-        this.handleRoundOver(alivePlayers);
         this.state = RoundState.ROUND_OVER;
+        this.handleRoundOver(alivePlayers);
         return true;
     }
 
     private handleRoundOver(alivePlayers: Player[]) {
-        this.#playerRanking = [...alivePlayers, ...this.#playerRanking];
+        alivePlayers.forEach((player) => {
+            this.addPlayerToRanking(player);
+        });
         this.emit({
             type: RoundEventTypes.ROUND_OVER,
             result: alivePlayers[0] ? 'PLAYER_WON' : 'DRAW',
             ranking: this.#playerRanking,
         });
+    }
+
+    private addPlayerToRanking(player: Player) {
+        this.#playerRanking[this.#currentRankIdx] = player;
+        this.#currentRankIdx--;
     }
 }
